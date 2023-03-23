1c1c3b1824cc1b89332a7c26f5c716e1713cbee8
Cedric <cedigasser@gmail.com>
2023-03-23 08:56:44 +0100

    Removed unused code

--- src/routes/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import grid from '$lib/classes/CharGrid'
-import type { ServerLoad } from '@sveltejs/kit';
-
-/** @type {import('@sveltejs/kit').Load} */
-export const load: ServerLoad = async () => {
-  return {
-    grid: grid.chars
-  }
-}
