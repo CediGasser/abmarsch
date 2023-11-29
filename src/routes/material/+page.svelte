308a51473d0b4f875f02737cca8d9b964771e1e6
Cedric <cedigasser@gmail.com>
2023-11-29 20:14:43 +0100

    remove unused page

--- src/routes/material/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  let items = ['Stgw 90', 'Tenue B Hose', 'Gamelle', 'Schutzmasken', 'Schutzbrille', 'GTE mit Taschen'];
-</script>
-
-<ul>
-  {#each items as item}
-    <li>{item}</li>
-  {/each}
-</ul>
