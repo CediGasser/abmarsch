2ac8f0cf745e40aee1a355223c3c800160822ce3
Cedric <cedigasser@gmail.com>
2023-11-29 20:25:51 +0100

    Add color-scheme to css

--- src/lib/components/DateInput.svelte
+++ src/lib/components/DateInput.svelte
@@ -1,31 +1,31 @@
 <script lang="ts">
-    import { onMount } from "svelte";
+  import { onMount } from 'svelte'
 
-    export let value: Date = new Date()
-    export let id: string | undefined = undefined
+  export let value: Date = new Date()
+  export let id: string | undefined = undefined
 
-    let input: HTMLInputElement
+  let input: HTMLInputElement
 
-    const dateChange = () => {
-        value = new Date(input.valueAsNumber)
-    }
+  const dateChange = () => {
+    value = new Date(input.valueAsNumber)
+  }
 
-    onMount(() => {
-        input.valueAsNumber = value.getTime()
-    })
+  onMount(() => {
+    input.valueAsNumber = value.getTime()
+  })
 </script>
 
 <input {id} type="date" bind:this={input} on:change={dateChange} />
 
 <style>
-    input {
-        display: inline;
-        color: var(--c-body);
-        padding-inline: var(--textFrameX);
-        padding-block: var(--textFrameY);
-        font-size: 1.5rem;
-        background-color: var(--c-overlay);
-        border: var(--fieldBorderWidth);
-        border-radius: var(--radius);
-    }
+  input {
+    display: inline;
+    color: var(--c-body);
+    padding-inline: var(--textFrameX);
+    padding-block: var(--textFrameY);
+    font-size: 1.5rem;
+    background-color: var(--c-overlay);
+    border: var(--fieldBorderWidth);
+    border-radius: var(--radius);
+  }
 </style>
