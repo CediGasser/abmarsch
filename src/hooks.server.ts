6b8ae29d409d64265cce3c9ef914a17dc7cd3160
Cedric <cedigasser@gmail.com>
2024-04-23 17:15:05 +0200

    chore: Add redirect hook

---
+++ src/hooks.server.ts
@@ -0,0 +1,14 @@
+export const handle = async ({ event, resolve }) => {
+  // redirect to canonical domain
+  if (event.url.hostname !== 'abmarsch.ch') {
+    return new Response(null, {
+      status: 301,
+      headers: {
+        Location: 'https://abmarsch.ch' + event.url.pathname,
+      },
+    })
+  }
+
+  const response = await resolve(event)
+  return response
+}
