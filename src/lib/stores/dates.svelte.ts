fe55477114b279ce004eb6de0cab57d9a7893670
Cedric <mail@cedricgasser.com>
2026-06-12 21:21:24 +0200

    Updated default dates

--- src/lib/stores/dates.svelte.ts
+++ src/lib/stores/dates.svelte.ts
@@ -1,7 +1,7 @@
 import { LocalDateStorage } from './storage.svelte'
 
-const DEFAULT_START_DATE = new Date('2026-01-12')
-const DEFAULT_END_DATE = new Date('2026-05-15')
+const DEFAULT_START_DATE = new Date('2026-06-29')
+const DEFAULT_END_DATE = new Date('2026-10-31')
 
 class Dates {
   #startDate = new LocalDateStorage('startDate', DEFAULT_START_DATE)
